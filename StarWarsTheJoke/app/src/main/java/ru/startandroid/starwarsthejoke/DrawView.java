d3590f03299f1c4bf88415bd5ef4c622525593fb
mobile <mobile@itcube.win>
2021-01-21 20:09:53 +0500

    Start Learning Of Dynamic View

---
+++ StarWarsTheJoke/app/src/main/java/ru/startandroid/starwarsthejoke/DrawView.java
@@ -0,0 +1,44 @@
+package ru.startandroid.starwarsthejoke;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
+    private DrawThread drawThread;
+    public DrawView(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        drawThread = new DrawThread(getContext(),getHolder());
+        drawThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // изменение размеров SurfaceView
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        drawThread.requestStop();
+        boolean retry = true;
+        while (retry) {
+            try {
+                drawThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                //
+            }
+        }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        drawThread.setTowardPoint((int)event.getX(),(int)event.getY());
+        return false;
+    }
+}
