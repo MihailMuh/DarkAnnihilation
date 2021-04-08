bc64a7aa1434c1dd08a43c6d665f480382442e23
MihailMuh <mukhortovm2004@mail.ru>
2021-04-09 01:13:45 +0500

    Adding First Background

--- app/src/main/java/ru/warfare/darkannihilation/MainActivity.java
+++ app/src/main/java/ru/warfare/darkannihilation/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         Objects.requireNonNull(getSupportActionBar()).hide();
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -72,8 +71,6 @@ public class MainActivity extends AppCompatActivity {
         );
         setContentView(R.layout.activity_main);
 
-        setVolumeControlStream(AudioManager.STREAM_MUSIC);
-
         WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         Display display = windowManager.getDefaultDisplay();
         Point size = new Point();
@@ -85,11 +82,11 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "" + e);
         }
 
-
         game = findViewById(R.id.gameView);
         game.initGame(size.x, size.y);
 
         checkOnFirstRun();
+        Log.e(TAG, "" + Runtime.getRuntime().maxMemory() / 1024);
     }
 
     @Override
